044c09228556a32cf20315b7787ebfdcd4ca36f6
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-05-07 17:56:50 +0200

    New way of collecting data, now calculates average price and quantity for given days perfectly

--- data_gathering_for_custom_items.py
+++ data_gathering_for_custom_items.py
@@ -1,108 +1,114 @@
+from collections import defaultdict
 import numpy as np
 from datetime import datetime
 from decouple import config
 import SteamMarket
+from time import sleep
 
 cookie = {"steamLoginSecure": config("STEAM_LOGIN_SECURE")}
 game_id = config("GAME_ID_1")
 SteamMarket.set_cookies(config("STEAM_LOGIN_SECURE"))
 SteamMarket.iniate_cookies()
 
-all_items_names = []
-current_run = 1
+all_items_names = [
+    "Sticker | FaZe Clan | Berlin 2019",
+    "Sticker | CR4ZY | Berlin 2019",
+    "Sticker | mousesports (Holo) | Berlin 2019",
+    "Sticker | FaZe Clan (Holo) | Berlin 2019",
+    "Sticker | Natus Vincere (Holo) | Berlin 2019",
+    "Sticker | compLexity Gaming | Berlin 2019",
+    "Sticker | mousesports (Foil) | Berlin 2019",
+    "Sticker | Team Liquid (Holo) | Berlin 2019",
+    "Sticker | Natus Vincere | Berlin 2019",
+    "Sticker | Team Liquid | Berlin 2019",
+    "Sticker | FaZe Clan (Foil) | Berlin 2019",
+    "Lt. Commander Ricksaw | NSWC SEAL",
+    "Sticker | ropz (Foil) | London 2018",
+    # "Operation Phoenix Weapon Case",
+    "Shattered Web Case",
+    "Galil AR | Akoben (Field-Tested)",
+    "SSG 08 | Bloodshot (Field-Tested)",
+    "SG 553 | Colony IV (Field-Tested)",
+    "AK-47 | Rat Rod (Minimal Wear)",
+    "AK-47 | Safari Mesh (Field-Tested)",
+    "CZ75-Auto | Emerald Quartz (Factory New)",
+    "MP9 | Stained Glass (Field-Tested)",
+    "MAC-10 | Copper Borre (Minimal Wear)",
+    "MAC-10 | Copper Borre (Factory New)",
+    "M4A1-S | Moss Quartz (Factory New)",
+]
 
-for item in all_items_names:
-    print(f"{str(current_run)} out of {str(len(all_items_names))}")
+
+def item_prices(item):
+    item_prices = defaultdict(list)
     print(f"DATA FOR: {item.upper()}")
-    current_run += 1
     item = SteamMarket.get_price_history(game_id, item)
     if item:
         item_price_data = item["prices"]
-        if not item_price_data:
-            continue
-        else:
-            item_prices = []
-            item_quantity = []
-            item_date = []
-            for current_day in item_price_data:
-                item_prices.append(current_day[1])
-                item_quantity.append(current_day[2])
-                item_date.append(datetime.strptime(current_day[0][0:11], "%b %d %Y"))
+        date = []
+        prices = []
+        quantity = []
+        for day in item_price_data:
+            date.append(datetime.strptime(day[0][0:11], "%b %d %Y"))
+            prices.append(day[1])
+            quantity.append(day[2])
+        for day in range(len(item_price_data) - 1, 1, -1):
+            index = 1
+            if prices[day] in item_prices[f'{date[day]}']:
+                continue
+            else:
+                for i in range(int(quantity[day])):
+                    item_prices[f'{date[day]}'].append(prices[day])
+            if date[day] == date[day - index]:
+                for i in range(int(quantity[day - index])):
+                    item_prices[f'{date[day]}'].append(prices[day - index])
+                index += 1
+            else:
+                continue
 
-            item_prices = list(map(float, item_prices))
-            item_quantity = list(map(int, item_quantity))
+    avg_price_today = round(np.mean(item_prices[list(item_prices.keys())[0]]), 3)
+    avg_price_5_days_ago = round(np.mean(item_prices[list(item_prices.keys())[5]]), 3)
+    avg_price_10_days_ago = round(np.mean(item_prices[list(item_prices.keys())[10]]), 3)
+    avg_price_30_days_ago = round(np.mean(item_prices[list(item_prices.keys())[30]]), 3)
 
-            for current_day in range(len(item_date) - 1, 1, -1):
-                index = 1
-                prices = []
-                for i in range(item_quantity[current_day]):
-                    prices.append(item_prices[current_day])
-                while item_date[current_day] == item_date[current_day - index]:
-                    for i in range(item_quantity[current_day - index]):
-                        prices.append(item_prices[current_day - index])
-                    item_quantity[current_day] = np.sum(
-                        [item_quantity[current_day], item_quantity[current_day - index]]
-                    )
-                    index += 1
-                else:
-                    item_prices[current_day] = np.mean(prices)
-                    break
+    print(f"Average price today: {avg_price_today} zł")
+    print(f'Average price 5 days ago: {avg_price_5_days_ago} zł')
+    print(f'Average price 10 days ago: {avg_price_10_days_ago} zł')
+    print(f'Average price 30 days ago: {avg_price_30_days_ago} zł')
 
-            avg_price_today = round(item_prices[-1], 3)
-            days_on_market = (datetime.today() - item_date[0]).days
-            price_change_from_day_1 = round(item_prices[-1] - item_prices[0], 3)
-            highest_price = max(item_prices)
-            lowest_price = min(item_prices)
-            highest_index = item_prices.index(highest_price)
-            lowest_index = item_prices.index(lowest_price)
-            diff_between_lowest_and_highest = round(highest_price - lowest_price, 3)
-            avg_price = round(np.mean(item_prices), 3)
-            quantity_today = item_quantity[-1]
 
-            price_change_past_5_days = 0
-            price_change_past_10_days = 0
-            price_change_past_30_days = 0
-            percentage_price_change_past_5_days = 0
-            percentage_price_change_past_10_days = 0
-            percentage_price_change_past_30_days = 0
+def item_quantity(item):
+    item_quantities = defaultdict(list)
+    item = SteamMarket.get_price_history(game_id, item)
+    if item:
+        item_price_data = item["prices"]
+        date = []
+        quantity = []
+        for day in item_price_data:
+            date.append(datetime.strptime(day[0][0:11], "%b %d %Y"))
+            quantity.append(day[2])
+        for day in range(len(item_price_data) - 1, 1, -1):
+            item_quantities[f'{date[day]}'].append(quantity[day])
+    item_quantities[list(item_quantities.keys())[0]] = list(map(int, item_quantities[list(item_quantities.keys())[0]]))
+    item_quantities[list(item_quantities.keys())[5]] = list(map(int, item_quantities[list(item_quantities.keys())[5]]))
+    item_quantities[list(item_quantities.keys())[10]] = list(map(int, item_quantities[list(item_quantities.keys())[10]]))
+    item_quantities[list(item_quantities.keys())[30]] = list(map(int, item_quantities[list(item_quantities.keys())[30]]))
+    quantity_today = np.sum(item_quantities[list(item_quantities.keys())[0]])
+    quantity_5_days_ago = np.sum(item_quantities[list(item_quantities.keys())[5]])
+    quantity_10_days_ago = np.sum(item_quantities[list(item_quantities.keys())[10]])
+    quantity_30_days_ago = np.sum(item_quantities[list(item_quantities.keys())[30]])
 
-            if item_prices[-5]:
-                price_change_past_5_days = round(item_prices[-1] - item_prices[-5], 3)
-                percentage_price_change_past_5_days = round(
-                    price_change_past_5_days / item_prices[-5] * 100, 2
-                )
-            if item_prices[-10]:
-                price_change_past_10_days = round(item_prices[-1] - item_prices[-10], 3)
-                percentage_price_change_past_10_days = round(
-                    price_change_past_10_days / item_prices[-10] * 100, 2
-                )
-            if item_prices[-30]:
-                price_change_past_30_days = round(item_prices[-1] - item_prices[-30], 3)
-                percentage_price_change_past_30_days = round(
-                    price_change_past_30_days / item_prices[-30] * 100, 2
-                )
-        print(f"Average price today: {avg_price_today} zł")
-        print(f"Days on the market: {days_on_market}")
-        print(
-            f"Price change from the 1st day on the market: {price_change_from_day_1} zł"
-        )
-        print(f"Highest price ever: {highest_price} zł")
-        print(f"Lowest price ever: {lowest_price} zł")
-        print(f"Highest difference in price: {diff_between_lowest_and_highest} zł")
-        print(f"Average price all time: {avg_price} zł")
-        print(f"Quantity sold today: {quantity_today}")
-        print(f"Price change in the past 5 days: {price_change_past_5_days} zł")
-        print(
-            f"Percentage price change in the past 5 days: {percentage_price_change_past_5_days}%"
-        )
-        print(f"Price change in the past 10 days: {price_change_past_10_days} zł")
-        print(
-            f"Percentage price change in the past 10 days: {percentage_price_change_past_10_days}%"
-        )
-        print(f"Price change in the past 30 days: {price_change_past_30_days} zł")
-        print(
-            f"Percentage price change in the past 30 days: {percentage_price_change_past_30_days}%"
-        )
-        print("<--- --->")
-        print("<--- --->")
-        print("<--- --->")
+    print(f"Quantity sold today: {quantity_today}")
+    print(f"Quantity sold 5 days ago: {quantity_5_days_ago}")
+    print(f"Quantity sold 10 days ago: {quantity_10_days_ago}")
+    print(f"Quantity sold 30 days ago: {quantity_30_days_ago}")
+    print("<--- --->")
+
+
+current_run = 1
+for i in all_items_names:
+    print(f"{str(current_run)} out of {str(len(all_items_names))}")
+    item_prices(i)
+    item_quantity(i)
+    current_run += 1
+    sleep(5)
