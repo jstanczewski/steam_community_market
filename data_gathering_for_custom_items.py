e5d5e96498e5984dde3b8b03718dec14b08afe23
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-05-09 18:11:47 +0200

    Added Telegram bot, now sends all the info via Telegram

--- data_gathering_for_custom_items.py
+++ data_gathering_for_custom_items.py
@@ -42,6 +42,7 @@ all_items_names = [
 def item_prices(item):
     item_prices = defaultdict(list)
     print(f"DATA FOR: {item.upper()}")
+    item_name = item
     item = SteamMarket.get_price_history(game_id, item)
     if item:
         item_price_data = item["prices"]
@@ -66,25 +67,34 @@ def item_prices(item):
             else:
                 continue
 
-    avg_price_today = round(np.mean(item_prices[list(item_prices.keys())[0]]), 3)
-    avg_price_yesterday = round(np.mean(item_prices[list(item_prices.keys())[1]]), 3)
-    avg_price_5_days_ago = round(np.mean(item_prices[list(item_prices.keys())[5]]), 3)
-    avg_price_10_days_ago = round(np.mean(item_prices[list(item_prices.keys())[10]]), 3)
-    avg_price_30_days_ago = round(np.mean(item_prices[list(item_prices.keys())[30]]), 3)
-    percentage_price_change_yesterday = round((avg_price_today - avg_price_yesterday) / avg_price_yesterday * 100, 2)
-    percentage_price_change_5_days = round((avg_price_today - avg_price_5_days_ago) / avg_price_5_days_ago * 100, 2)
-    percentage_price_change_10_days = round((avg_price_today - avg_price_10_days_ago) / avg_price_10_days_ago * 100, 2)
-    percentage_price_change_30_days = round((avg_price_today - avg_price_30_days_ago) / avg_price_30_days_ago * 100, 2)
+    avg_price_today = round(np.mean(item_prices[list(item_prices.keys())[0]]), 2)
+    avg_price_yesterday = round(np.mean(item_prices[list(item_prices.keys())[1]]), 2)
+    avg_price_5_days_ago = round(np.mean(item_prices[list(item_prices.keys())[5]]), 2)
+    avg_price_10_days_ago = round(np.mean(item_prices[list(item_prices.keys())[10]]), 2)
+    avg_price_30_days_ago = round(np.mean(item_prices[list(item_prices.keys())[30]]), 2)
+    percentage_price_change_yesterday = round(
+        (avg_price_today - avg_price_yesterday) / avg_price_yesterday * 100, 2
+    )
+    percentage_price_change_5_days = round(
+        (avg_price_today - avg_price_5_days_ago) / avg_price_5_days_ago * 100, 2
+    )
+    percentage_price_change_10_days = round(
+        (avg_price_today - avg_price_10_days_ago) / avg_price_10_days_ago * 100, 2
+    )
+    percentage_price_change_30_days = round(
+        (avg_price_today - avg_price_30_days_ago) / avg_price_30_days_ago * 100, 2
+    )
 
     print(f"Average price today: {avg_price_today} zł")
     print(f"Average price yesterday: {avg_price_yesterday} zł")
-    print(f'Percentage price change: {percentage_price_change_yesterday}%')
+    print(f"Percentage price change: {percentage_price_change_yesterday}%")
     print(f"Average price 5 days ago: {avg_price_5_days_ago} zł")
-    print(f'Percentage price change: {percentage_price_change_5_days}%')
+    print(f"Percentage price change: {percentage_price_change_5_days}%")
     print(f"Average price 10 days ago: {avg_price_10_days_ago} zł")
-    print(f'Percentage price change: {percentage_price_change_10_days}%')
+    print(f"Percentage price change: {percentage_price_change_10_days}%")
     print(f"Average price 30 days ago: {avg_price_30_days_ago} zł")
-    print(f'Percentage price change: {percentage_price_change_30_days}%')
+    print(f"Percentage price change: {percentage_price_change_30_days}%")
+    return f"Item name: {item_name}. \n\nAverage price today: {avg_price_today} zł \n\nAverage price yesterday: {avg_price_yesterday} zł \nPercentage price change: {percentage_price_change_yesterday}% \n\nAverage price 5 days ago: {avg_price_5_days_ago} zł \nPercentage price change: {percentage_price_change_5_days}% \n\nAverage price 10 days ago: {avg_price_10_days_ago} zł \nPercentage price change: {percentage_price_change_10_days}% \n\nAverage price 30 days ago: {avg_price_30_days_ago} zł \nPercentage price change: {percentage_price_change_30_days}% \n"
 
 
 def item_quantity(item):
@@ -102,6 +112,9 @@ def item_quantity(item):
     item_quantities[list(item_quantities.keys())[0]] = list(
         map(int, item_quantities[list(item_quantities.keys())[0]])
     )
+    item_quantities[list(item_quantities.keys())[1]] = list(
+        map(int, item_quantities[list(item_quantities.keys())[1]])
+    )
     item_quantities[list(item_quantities.keys())[5]] = list(
         map(int, item_quantities[list(item_quantities.keys())[5]])
     )
@@ -112,18 +125,21 @@ def item_quantity(item):
         map(int, item_quantities[list(item_quantities.keys())[30]])
     )
     quantity_today = np.sum(item_quantities[list(item_quantities.keys())[0]])
+    quantity_yesterday = np.sum(item_quantities[list(item_quantities.keys())[1]])
     quantity_5_days_ago = np.sum(item_quantities[list(item_quantities.keys())[5]])
     quantity_10_days_ago = np.sum(item_quantities[list(item_quantities.keys())[10]])
     quantity_30_days_ago = np.sum(item_quantities[list(item_quantities.keys())[30]])
 
     print(f"Quantity sold today: {quantity_today}")
+    print(f"Quantity sold yesterday: {quantity_yesterday}")
     print(f"Quantity sold 5 days ago: {quantity_5_days_ago}")
     print(f"Quantity sold 10 days ago: {quantity_10_days_ago}")
     print(f"Quantity sold 30 days ago: {quantity_30_days_ago}")
     print("<--- --->")
+    return f"Quantity sold today: {quantity_today} \nQuantity sold yesterday: {quantity_yesterday} \nQuantity sold 5 days ago: {quantity_5_days_ago} \nQuantity sold 10 days ago: {quantity_10_days_ago} \nQuantity sold 30 days ago: {quantity_30_days_ago}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     current_run = 1
     for i in all_items_names:
         print(f"{str(current_run)} out of {str(len(all_items_names))}")
---
+++ telegram_bot.py
@@ -0,0 +1,78 @@
+from time import sleep
+from decouple import config
+import requests
+from data_gathering_for_custom_items import item_prices, item_quantity
+
+item_names = [
+    "Sticker | FaZe Clan | Berlin 2019",
+    "Sticker | CR4ZY | Berlin 2019",
+    "Sticker | mousesports (Holo) | Berlin 2019",
+    "Sticker | FaZe Clan (Holo) | Berlin 2019",
+    "Sticker | Natus Vincere (Holo) | Berlin 2019",
+    "Sticker | compLexity Gaming | Berlin 2019",
+    "Sticker | mousesports (Foil) | Berlin 2019",
+    "Sticker | Team Liquid (Holo) | Berlin 2019",
+    "Sticker | Natus Vincere | Berlin 2019",
+    "Sticker | Team Liquid | Berlin 2019",
+    "Sticker | FaZe Clan (Foil) | Berlin 2019",
+    "Lt. Commander Ricksaw | NSWC SEAL",
+    "Sticker | ropz (Foil) | London 2018",
+    # "Operation Phoenix Weapon Case",
+    "Shattered Web Case",
+    "Galil AR | Akoben (Field-Tested)",
+    "SSG 08 | Bloodshot (Field-Tested)",
+    "SG 553 | Colony IV (Field-Tested)",
+    "AK-47 | Rat Rod (Minimal Wear)",
+    "AK-47 | Safari Mesh (Field-Tested)",
+    "CZ75-Auto | Emerald Quartz (Factory New)",
+    "MP9 | Stained Glass (Field-Tested)",
+    "MAC-10 | Copper Borre (Minimal Wear)",
+    "MAC-10 | Copper Borre (Factory New)",
+    "M4A1-S | Moss Quartz (Factory New)",
+]
+
+bot_api_key = config("BOT_API_KEY")
+chat_id = config("CHAT_ID")
+while True:
+    for item in item_names:
+        text = item_prices(item) + "\n" + item_quantity(item)
+        text = text.replace("%", "%25").replace(" ", "%20").replace("&", "%26")
+        http = (
+            "https://api.telegram.org/bot"
+            + bot_api_key
+            + "/sendMessage?chat_id="
+            + chat_id
+            + "&text="
+            + text
+        )
+
+        requests.get(http)
+
+        # text = item_quantity(item)
+        # text = text.replace("%", "%25").replace(" ", "%20").replace("&", "%26")
+        # http = (
+        #     "https://api.telegram.org/bot"
+        #     + bot_api_key
+        #     + "/sendMessage?chat_id="
+        #     + chat_id
+        #     + "&text="
+        #     + text
+        # )
+        #
+        # requests.get(http)
+
+        sleep(5)
+    sleep(60)
+
+
+# for _ in range(20):
+#     text = '.'
+#     http = (
+#             "https://api.telegram.org/bot"
+#             + bot_api_key
+#             + "/sendMessage?chat_id="
+#             + chat_id
+#             + "&text="
+#             + text
+#         )
+#     requests.get(http)
